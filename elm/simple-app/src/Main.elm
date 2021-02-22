0b2f638d94450ae7316b23d386ed9008bcd671c8
Anders <anders@kalhauge.dk>
2021-02-22 15:46:15 +0100

    get and set Pet

--- elm/simple-app/src/Main.elm
+++ elm/simple-app/src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, input, span, text)
+import Html exposing (Html, button, div, input, li, span, text, ul)
 import Html.Attributes exposing (style, value)
 import Html.Events exposing (onClick, onInput)
 
@@ -16,37 +16,76 @@ type alias Pet =
     , alive: Bool
     }
 
-init : Pet
-init = Pet 7 "Ninus" 9 True
+type alias Model =
+    { pets: List Pet }
+
+init : Model
+init = Model
+    [ Pet 7 "Ninus" 9 True
+    , Pet 9 "Rufus" 3 True
+    , Pet 13 "Felix" 5 True
+    ]
 
 -- UPDATE
 
 type Message
-    = CelebratePet
+    = CelebratePet Int
     | KillPet
     | NameChanged String
 
-update : Message -> Pet -> Pet
-update message pet =
+getPet : Int -> List Pet -> Maybe Pet
+getPet id pets =
+    case pets of
+        [] -> Nothing
+        pet :: rest -> if pet.id == id then Just pet else (getPet id rest)
+
+setPet : Pet -> List Pet -> List Pet
+setPet pet pets =
+    case pets of
+        [] -> [pet]
+        p :: rest -> if pet.id == p.id then pet :: rest else p :: (setPet pet rest)
+
+update : Message -> Model -> Model
+update message model =
     case message of
-        CelebratePet ->
-            { pet | age = (pet.age + 1) }
+        CelebratePet id ->
+            let
+                maybePet = getPet id model.pets
+            in
+                case maybePet of
+                    Just pet ->
+                        let
+                            updatedPet = { pet | age = pet.age + 1 }
+                        in
+                            { model | pets = (setPet updatedPet model.pets) }
+                    Nothing ->
+                        model
+            -- { pet | age = (pet.age + 1) }
+        _ -> model
+{-
         KillPet ->
             { pet | alive = False }
         NameChanged newName ->
             { pet | name = newName }
+-}
 
 -- VIEW
 
-view : Pet -> Html Message
-view pet =
+view : Model -> Html Message
+view model =
     div [ style "margin" "2em" ]
+        [ ul [] (List.map viewPet model.pets)
+        ]
+
+viewPet : Pet -> Html Message
+viewPet pet =
+    li []
         [ span [ style "marginRight" "1ex"] [ text (String.fromInt pet.id) ]
         , input [ onInput NameChanged,  value pet.name ] []
         , span
             [ style "color" (if pet.alive then "green" else "red") ]
             [ text (" is "++(String.fromInt pet.age)++" years old") ]
-        , button [ onClick CelebratePet ] [ text "Celebrate" ]
+        , button [ onClick (CelebratePet pet.id)] [ text "Celebrate" ]
         , button [ onClick KillPet ] [text "Kill" ]
         ]
 
