e2dc21fd95b2c123e57ce1e277c57b6a2f9f0c63
Anders <anders@kalhauge.dk>
2021-02-22 14:38:46 +0100

    onClick and onInput

--- elm/simple-app/src/Main.elm
+++ elm/simple-app/src/Main.elm
@@ -1,8 +1,9 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, text)
-import Html.Attributes exposing (style)
+import Html exposing (Html, button, div, input, span, text)
+import Html.Attributes exposing (style, value)
+import Html.Events exposing (onClick, onInput)
 
 main = Browser.sandbox { init = init, update = update, view = view }
 
@@ -23,6 +24,7 @@ init = Pet 7 "Ninus" 9 True
 type Message
     = CelebratePet
     | KillPet
+    | NameChanged String
 
 update : Message -> Pet -> Pet
 update message pet =
@@ -31,10 +33,24 @@ update message pet =
             { pet | age = (pet.age + 1) }
         KillPet ->
             { pet | alive = False }
+        NameChanged newName ->
+            { pet | name = newName }
 
 -- VIEW
+
 view : Pet -> Html Message
 view pet =
-    div [ style "color" "red"] [ text pet.name ]
+    div [ style "margin" "2em" ]
+        [ span [ style "marginRight" "1ex"] [ text (String.fromInt pet.id) ]
+        , input [ onInput NameChanged,  value pet.name ] []
+        , span
+            [ style "color" (if pet.alive then "green" else "red") ]
+            [ text (" is "++(String.fromInt pet.age)++" years old") ]
+        , button [ onClick CelebratePet ] [ text "Celebrate" ]
+        , button [ onClick KillPet ] [text "Kill" ]
+        ]
+
+--    div [ style "color" "red"] [ text ("Min kanin hedder "++pet.name) ]
 
 -- <div style="color: red">Ninus</div>
+-- <button onclick="...">Celebrate</button>
